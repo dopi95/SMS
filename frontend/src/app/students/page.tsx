dcf468d285619a6fbe213a52c33c0254fb40482c
dopi95 <elyasyenealem@gmail.com>
2026-01-30 18:21:18 +0300

    Fix syntax error in students page

--- frontend/src/app/students/page.tsx
+++ frontend/src/app/students/page.tsx
@@ -587,7 +587,7 @@ export default function StudentsPage() {
                         </div>
                       </div>
                     </div>
-                  ))}}
+                  ))}
                 </div>
               )}
             </div>
